9a87ab9389771f20948039315f99a75ffd63864c
John Quesada <john.quesada@sevensoftit.com>
2019-01-04 18:58:11 -0600

    update plugin

--- src/android/OnfidoIdCheck.java
+++ src/android/OnfidoIdCheck.java
@@ -64,7 +64,7 @@ public class OnfidoIdCheck extends CordovaPlugin {
             this.substract(args, callbackContext);
             return true;
         } else if (action.equals("startSdk")) {
-            final Context context = this.cordova.getActivity();
+            final Activity context = this.cordova.getActivity();
             client = OnfidoFactory.create(context).getCliente();
             this.startSdk(args, callbackContext);
             return true;
