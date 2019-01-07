28ab9fe3a099501daa75b3d4ba0085aff5cc9a34
John Quesada <john.quesada@sevensoftit.com>
2019-01-07 10:26:49 -0600

    actualización de colores

--- src/android/OnfidoIdCheck.java
+++ src/android/OnfidoIdCheck.java
@@ -1,6 +1,5 @@
 package cordova.plugin.onfido;
 
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -10,7 +9,6 @@ import org.apache.cordova.PluginResult.Status;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
-
 import com.onfido.android.sdk.capture.ExitCode;
 import com.onfido.android.sdk.capture.Onfido;
 import com.onfido.android.sdk.capture.OnfidoConfig;
@@ -26,12 +24,10 @@ import com.onfido.api.client.data.Applicant;
 import com.onfido.api.client.data.Check;
 import com.onfido.api.client.data.ErrorData;
 import com.onfido.api.client.data.Report;
-
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
-
 import android.util.Log;
 import android.app.Activity;
 
@@ -51,17 +47,18 @@ public class OnfidoIdCheck extends CordovaPlugin {
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if(action.equals("startSdk")) {
-            Activity context=this.cordova.getActivity();
+            Activity context = this.cordova.getActivity();
             client = OnfidoFactory.create(context).getClient();
 
             final FlowStep[] defaultStepsWithWelcomeScreen = new FlowStep[]{
                     FlowStep.WELCOME,
-                    FlowStep.CAPTURE_DOCUMENT,
+                    new CaptureScreenStep(DocumentType.PASSPORT, CountryCode.SV),//FlowStep.CAPTURE_DOCUMENT,
                     FlowStep.CAPTURE_FACE,
                     FlowStep.FINAL
             };
 
             OnfidoConfig onfidoConfig = OnfidoConfig.builder()
+                    .withCustomFlow(defaultStepsWithWelcomeScreen)
                     .withToken("YOUR_MOBILE_TOKEN")
                     .withApplicant("YOUR_APPLICANT_ID")
                     .build();
