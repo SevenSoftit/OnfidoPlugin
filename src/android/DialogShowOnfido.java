144e65c09394243c187e92cb5961200d834c8cd8
John Quesada <john.quesada@sevensoftit.com>
2019-01-09 13:06:17 -0600

    update

---
+++ src/android/DialogShowOnfido.java
@@ -0,0 +1,194 @@
+package cordova.plugin.onfido;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.onfido.android.sdk.capture.DocumentType;
+import com.onfido.android.sdk.capture.ExitCode;
+import com.onfido.android.sdk.capture.Onfido;
+import com.onfido.android.sdk.capture.OnfidoConfig;
+import com.onfido.android.sdk.capture.OnfidoFactory;
+import com.onfido.android.sdk.capture.errors.OnfidoException;
+import com.onfido.android.sdk.capture.ui.BaseActivity;
+import com.onfido.android.sdk.capture.ui.options.CaptureScreenStep;
+import com.onfido.android.sdk.capture.ui.options.FlowStep;
+import com.onfido.android.sdk.capture.ui.options.MessageScreenStep;
+import com.onfido.android.sdk.capture.upload.Captures;
+import com.onfido.android.sdk.capture.utils.CountryCode;
+import com.onfido.api.client.data.Applicant;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DialogShowOnfido extends BaseActivity {
+
+    private Onfido client;
+    private String applicantId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = OnfidoFactory.create(this).getClient();
+        setWelcomeScreen();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        client.handleActivityResult(resultCode, data, new Onfido.OnfidoResultListener() {
+            @Override
+            public void userCompleted(Applicant applicant, Captures captures) {
+                startCheck(applicant);
+            }
+
+            @Override
+            public void userExited(ExitCode exitCode, Applicant applicant) {
+                showToast("User cancelled.");
+            }
+
+            @Override
+            public void onError(OnfidoException e, Applicant applicant) {
+                e.printStackTrace();
+                showToast("Unknown error");
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void startCheck(Applicant applicant) {
+        //Call your back end to initiate the check
+        //completedCheck();
+        completedCheck(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    showToast("onResponse");
+                    String v = response.toString();
+                    applicantId = response.getString("id");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(ANError anError) {
+                ANError v = anError;
+                showToast("onError");
+            }
+        });
+    }
+
+    private void setWelcomeScreen() {
+        setContentView(R.layout.activity_main);
+
+        final FlowStep[] flowStepsWithOptions = new FlowStep[]{
+                new MessageScreenStep("Welcome", "In the following steps you will be asked to perform a verification check", "Start"),
+                new CaptureScreenStep(DocumentType.PASSPORT, CountryCode.SV),
+                FlowStep.CAPTURE_FACE,
+                new MessageScreenStep("Thank you", "We will use your captured document and face to perform a verification check", "Start Check")
+        };
+
+        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFlow(flowStepsWithOptions);
+            }
+        });
+    }
+
+    private void startFlow(final FlowStep[] flowSteps) {
+        createApplicant(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    applicantId = response.getString("id");
+
+                    OnfidoConfig.Builder onfidoConfigBuilder = OnfidoConfig.builder().withApplicant(applicantId).withToken("test_iCPCbZOQv01rBCSZ5xZt65JaqMj_et76");
+
+                    if (flowSteps != null) {
+                        onfidoConfigBuilder.withCustomFlow(flowSteps);
+                    }
+
+                    OnfidoConfig onfidoConfig = onfidoConfigBuilder.build();
+                    client.startActivityForResult(MainActivity.this, 1, onfidoConfig);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(ANError anError) {
+            }
+        });
+    }
+
+    private void createApplicant(JSONObjectRequestListener listener) {
+        try {
+
+            /*
+            $ curl https://api.onfido.com/v2/applicants \
+              -H 'Authorization: Token token=YOUR_API_TOKEN' \
+              -d 'first_name=Theresa' \
+              -d 'last_name=May'
+             */
+            String token = "test_BCoZn8ZVcYtYhTAq77Tt2h9u0I9OX75R";//getString(R.string.onfido_api_token);
+            final JSONObject applicant = new JSONObject();
+            applicant.put("first_name", "Theresa");
+            applicant.put("last_name", "May");
+
+            AndroidNetworking.post("https://api.onfido.com/v2/applicants")
+                    .addJSONObjectBody(applicant)
+                    .addHeaders("Accept", "application/json")
+                    .addHeaders("Authorization", "Token token=" + token)
+                    .build()
+                    .getAsJSONObject(listener);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void completedCheck(JSONObjectRequestListener listener) {
+        try {
+            /*
+            $ curl https://api.onfido.com/v2/applicants/YOUR_APPLICANT_ID/checks \
+            -H 'Authorization: Token token=YOUR_API_TOKEN' \
+            -d 'type=express' \
+            -d 'reports[][name]=document' \
+            -d 'reports[][name]=facial_similarity' \
+            -d 'reports[][variant]=standard'
+            */
+            //showToast("completeCheck");
+
+            String token = "test_BCoZn8ZVcYtYhTAq77Tt2h9u0I9OX75R";//getString(R.string.onfido_api_token);
+            final JSONObject applicant = new JSONObject();
+            applicant.put("type", "express");
+            applicant.put("reports","['name': 'document']");
+            //applicant.put("reports", "['name': 'facial_similarity'']");
+            applicant.put("reports", "['variant': 'standard']");
+
+            AndroidNetworking.post("https://api.onfido.com/v2/applicants/" + this.applicantId + "/checks")
+                    .addJSONObjectBody(applicant)
+                    .addHeaders("Accept", "application/json")
+                    .addHeaders("Authorization", "Token token=" + token)
+                    .build()
+                    .getAsJSONObject(listener);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/android/OnfidoIdCheck.java
+++ src/android/OnfidoIdCheck.java
@@ -58,12 +58,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class OnfidoIdCheck extends CordovaPlugin {
-    private static final String TAG = "OnfidoIdCheck";
-
+    private static final String TAG = "MyCordovaPlugin";
     private Onfido client;
     private OnfidoAPI onfidoAPI;
-    private Activity context;
-    private String applicantId;
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -72,286 +69,43 @@ public class OnfidoIdCheck extends CordovaPlugin {
     }
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if (action.equals("startSdk")) {
-            context = this.cordova.getActivity();
-            client = OnfidoFactory.create(context).getClient();
-            
-            setWelcomeScreen();
-            /*
-            context = this.cordova.getActivity();
+        if(action.equals("startSdk")) {
+            Activity context=this.cordova.getActivity();
             client = OnfidoFactory.create(context).getClient();
 
             final FlowStep[] defaultStepsWithWelcomeScreen = new FlowStep[]{
-                    new MessageScreenStep("Welcome", "In the following steps you will be asked to perform a verification check", "Start"),//FlowStep.WELCOME,
-                    new CaptureScreenStep(DocumentType.PASSPORT, CountryCode.SV),
+                    FlowStep.WELCOME,
+                    FlowStep.CAPTURE_DOCUMENT,
                     FlowStep.CAPTURE_FACE,
-                    new MessageScreenStep("Thank you", "We will use your captured document and face to perform a verification check", "Start Check")
+                    FlowStep.FINAL
             };
 
-            createApplicant(new JSONObjectRequestListener() {
-                //  showToast("createApplicant");
+            OnfidoConfig onfidoConfig = getTestOnfidoConfigBuilder()
+                    .withCustomFlow(defaultStepsWithWelcomeScreen)
+                    .build();
 
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        String applicantId = response.getString("id");
-                        showToast("onResponse");
-                        showToast(applicantId);
-                        OnfidoConfig onfidoConfig = OnfidoConfig.builder()
-                                .withCustomFlow(defaultStepsWithWelcomeScreen)
-                                .withToken("test_iCPCbZOQv01rBCSZ5xZt65JaqMj_et76")
-                                .withApplicant(applicantId)
-                                .build();
+            onfidoAPI = OnfidoApiUtil.createOnfidoApiClient(context, onfidoConfig);
 
-                        onfidoAPI = OnfidoApiUtil.createOnfidoApiClient(context, onfidoConfig);
+            client.startActivityForResult(context, 1, onfidoConfig);
 
-                        client.startActivityForResult(context, 1, onfidoConfig);
-
-                        // An example of returning data back to the web layer
-                        final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
-                        callbackContext.sendPluginResult(result);
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        showToast("onResponse Unknown error");
-                    }
-                }
-
-                @Override
-                public void onError(ANError anError) {
-                }
-            });
-            */
+            // An example of returning data back to the web layer
+            final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
+            callbackContext.sendPluginResult(result);
         }
         return true;
     }
 
-    private void startCheck(Applicant applicant) {
-        //Call your back end to initiate the check
-        //completedCheck();
-        completedCheck(new JSONObjectRequestListener() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    showToast("onResponse");
-                    String v = response.toString();
-                    applicantId = response.getString("id");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onError(ANError anError) {
-                ANError v = anError;
-                showToast("onError startCheck");
-            }
-        });
+    private Applicant getTestApplicant() {
+        return Applicant.builder()
+                .withFirstName("Ionic")
+                .withLastName("User")
+                //.withToken("YOUR_MOBILE_TOKEN")
+                .build();
     }
 
-    private void setWelcomeScreen() {
-        //setContentView(R.layout.activity_main);
-
-        final FlowStep[] flowStepsWithOptions = new FlowStep[]{
-                new MessageScreenStep("Welcome", "In the following steps you will be asked to perform a verification check", "Start"),
-                new CaptureScreenStep(DocumentType.PASSPORT, CountryCode.SV),
-                FlowStep.CAPTURE_FACE,
-                new MessageScreenStep("Thank you", "We will use your captured document and face to perform a verification check", "Start Check")
-        };
-
-        startFlow(flowStepsWithOptions);
+    private OnfidoConfig.Builder getTestOnfidoConfigBuilder() {
+        return OnfidoConfig.builder()
+                .withToken("test_iCPCbZOQv01rBCSZ5xZt65JaqMj_et76")
+                .withApplicant(getTestApplicant());
     }
-
-    private void startFlow(final FlowStep[] flowSteps) {
-        createApplicant(new JSONObjectRequestListener() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    applicantId = response.getString("id");
-
-                    OnfidoConfig.Builder onfidoConfigBuilder = OnfidoConfig.builder().withApplicant(applicantId).withToken("test_iCPCbZOQv01rBCSZ5xZt65JaqMj_et76");
-
-                    if (flowSteps != null) {
-                        onfidoConfigBuilder.withCustomFlow(flowSteps);
-                    }
-
-                    OnfidoConfig onfidoConfig = onfidoConfigBuilder.build();
-                    client.startActivityForResult(context, 1, onfidoConfig);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onError(ANError anError) {
-                showToast("onError startFlow");
-            }
-        });
-    }
-
-    private void createApplicant(JSONObjectRequestListener listener) {
-        try {
-            String token = "test_BCoZn8ZVcYtYhTAq77Tt2h9u0I9OX75R";//getString(R.string.onfido_api_token);
-            final JSONObject applicant = new JSONObject();
-            applicant.put("first_name", "Theresa");
-            applicant.put("last_name", "May");
-
-            AndroidNetworking.post("https://api.onfido.com/v2/applicants")
-                    .addJSONObjectBody(applicant)
-                    .addHeaders("Accept", "application/json")
-                    .addHeaders("Authorization", "Token token=" + token)
-                    .build()
-                    .getAsJSONObject(listener);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-            showToast("Unknown error");
-        }
-    }
-
-    private void completedCheck(JSONObjectRequestListener listener) {
-        try {
-            /*
-            $ curl https://api.onfido.com/v2/applicants/YOUR_APPLICANT_ID/checks \
-            -H 'Authorization: Token token=YOUR_API_TOKEN' \
-            -d 'type=express' \
-            -d 'reports[][name]=document' \
-            -d 'reports[][name]=facial_similarity' \
-            -d 'reports[][variant]=standard'
-            */
-            //showToast("completeCheck");
-
-            String token = "test_BCoZn8ZVcYtYhTAq77Tt2h9u0I9OX75R";//getString(R.string.onfido_api_token);
-            final JSONObject applicant = new JSONObject();
-            applicant.put("type", "express");
-            applicant.put("reports", "['name': 'document']");
-            //applicant.put("reports", "['name': 'facial_similarity'']");
-            applicant.put("reports", "['variant': 'standard']");
-
-            AndroidNetworking.post("https://api.onfido.com/v2/applicants/" + this.applicantId + "/checks")
-                    .addJSONObjectBody(applicant)
-                    .addHeaders("Accept", "application/json")
-                    .addHeaders("Authorization", "Token token=" + token)
-                    .build()
-                    .getAsJSONObject(listener);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-    }
-
-    /*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        client.handleActivityResult(resultCode, data, new Onfido.OnfidoResultListener() {
-            @Override
-            public void userCompleted(Applicant applicant, Captures captures) {
-                startCheck(applicant);
-            }
-
-            @Override
-            public void userExited(ExitCode exitCode, Applicant applicant) {
-                showToast("User cancelled.");
-            }
-
-            @Override
-            public void onError(OnfidoException e, Applicant applicant) {
-                e.printStackTrace();
-                showToast("Unknown error");
-            }
-        });
-    }
-  /*
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    if (action.equals("add")) {
-      this.add(args, callbackContext);
-      return true;
-    } else if (action.equals("multiply")) {
-      this.multiply(args, callbackContext);
-      return true;
-    } else if (action.equals("divide")) {
-      this.divide(args, callbackContext);
-      return true;
-    } else if (action.equals("substract")) {
-      this.substract(args, callbackContext);
-      return true;
-    } else if (action.equals("startSdk")) {
-      final Activity context = this.cordova.getActivity();
-      client = OnfidoFactory.create(context).getCliente();
-      this.startSdk(args, callbackContext);
-      return true;
-    }
-    return false;
-  }
-
-  private void add(JSONArray args, CallbackContext callbackContext) {
-    if (args != null) {
-      try {
-        int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-        int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-        callbackContext.success("" + (p1 + p2));
-      } catch (Exception e) {
-        callbackContext.error("Invalid add operation");
-      }
-
-    } else {
-      callbackContext.error("Expected one non-empty string argument.");
-    }
-  }
-
-  private void multiply(JSONArray args, CallbackContext callbackContext) {
-    if (args != null) {
-      try {
-        int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-        int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-        callbackContext.success("" + (p1 * p2));
-      } catch (Exception e) {
-        callbackContext.error("Invalid multiply operation");
-      }
-
-    } else {
-      callbackContext.error("Expected one non-empty string argument.");
-    }
-  }
-
-  private void substract(JSONArray args, CallbackContext callbackContext) {
-    if (args != null) {
-      try {
-        int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-        int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-        callbackContext.success("" + (p1 - p2));
-      } catch (Exception e) {
-        callbackContext.error("Invalid substract operation");
-      }
-
-    } else {
-      callbackContext.error("Expected one non-empty string argument.");
-    }
-  }
-
-  private void divide(JSONArray args, CallbackContext callbackContext) {
-    if (args != null) {
-      try {
-        int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-        int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-        callbackContext.success("" + (p1 / p2));
-      } catch (Exception e) {
-        callbackContext.error("Invalid divide operation");
-      }
-
-    } else {
-      callbackContext.error("Expected one non-empty string argument.");
-    }
-  }
-
-
-    */
 }
