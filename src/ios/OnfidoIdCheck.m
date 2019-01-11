7f854d7277c8bd939a8ae7c24663865f31e12dc7
jquesada7soft <45218121+jquesada7soft@users.noreply.github.com>
2019-01-11 15:35:12 -0600

    Agregar CLASES IOS
    Se agregan archivos necesarios para IOS.

--- src/ios/OnfidoIdCheck.m
+++ src/ios/OnfidoIdCheck.m
@@ -1,109 +1,22 @@
-/********* OnfidoIdCheck.m Cordova Plugin Implementation *******/
+#import "OnfidoIdCheck.h"
 
-#import <Cordova/CDV.h>
+#import <Cordova/CDVAvailability.h>
 
-@interface OnfidoIdCheck : CDVPlugin {
-// Member variables go here.
+@implementation OnfidoIdCheck
+
+- (void)pluginInitialize {
 }
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
-- (void)add:(CDVInvokedUrlCommand*)command;
-- (void)multiply:(CDVInvokedUrlCommand*)command;
-- (void)substract:(CDVInvokedUrlCommand*)command;
-- (void)divide:(CDVInvokedUrlCommand*)command;
-
-    @end
-
-    @implementation OnfidoIdCheck
-
-    - (void)add:(CDVInvokedUrlCommand*)command
-{
-  CDVPluginResult* pluginResult = nil;
-
-  NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-if (param1 >=0 && param2 >= 0)
+- (void)startSdk:(CDVInvokedUrlCommand*)command
 {
 
-  NSString* total = @(param1 + param2);
-pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
+    NSString* name = [[command arguments] objectAtIndex:0];
+    NSString* msg = [NSString stringWithFormat: @"Hola todo el...  %@", name];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:msg];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
-else
-{
-  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
-}
-
-[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-}
-
-- (void)substract:(CDVInvokedUrlCommand*)command
-{
-  CDVPluginResult* pluginResult = nil;
-
-  NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-if (param1 >=0 && param2 >= 0)
-{
-
-  NSString* total = @(param1 - param2);
-pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
-}
-else
-{
-  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
-}
-
-[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-}
-
-- (void)multiply:(CDVInvokedUrlCommand*)command
-{
-  CDVPluginResult* pluginResult = nil;
-
-  NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-if (param1 >=0 && param2 >= 0)
-{
-
-  NSString* total = @(param1 * param2);
-pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
-}
-else
-{
-  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
-}
-
-[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-}
-- (void)divide:(CDVInvokedUrlCommand*)command
-{
-  CDVPluginResult* pluginResult = nil;
-
-  NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-if (param1 >=0 && param2 >= 0)
-{
-
-  NSString* total = @(param1 / param2);
-pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
-}
-else
-{
-  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
-}
-
-[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-}
-
-
 
 @end
-
